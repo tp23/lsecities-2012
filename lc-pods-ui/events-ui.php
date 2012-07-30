fed2f49a1d6a547ed83172a95d4e5eeec0d6e8ff
andrea rota <a.rota@lse.ac.uk>
2012-07-30 11:50:15 +0100

    added UI for events

---
+++ lc-pods-ui/events-ui.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: LSE Cities Events Pods UI
+Plugin URI: http://lsecities.net/labs/
+Description: Customized Pods UI for Events
+Version: 0.2.1
+Author: Andrea Rota
+Author URI: http://lsecities.net/
+*/
+
+function pods_ui_events()
+{
+  $icon = '';
+  add_object_page('Events', 'Events', 'read', 'pods-events', '', $icon);
+  add_submenu_page('pods-events', 'Events', 'Events', 'read', 'pods-events', 'event_page');
+}
+
+function event_page()
+{
+  $object = new Pod('event');
+  $object->ui = array(
+    'title'   => 'Event',
+    'columns' => array(
+      'name'       => 'Title',
+      'date_start' => 'Date',
+      'venue'      => 'Venue',
+      'speakers'   => 'Speakers',
+      'created'    => 'Date Created',
+      'modified'   => 'Last Modified'
+    ),
+  );
+  pods_ui_manage($object);
+}
+
+add_action('admin_menu','pods_ui_events');
+
+?>
