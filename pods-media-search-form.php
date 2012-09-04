8dbb85795243c4706fb11442b40006439db25aee
andrea rota <a.rota@lse.ac.uk>
2012-09-04 10:29:11 +0100

    added template

---
+++ pods-media-search-form.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Template Name: Pods - Media Archive search
+ * Description: Media Archive search webapp
+ *
+ * @package LSE Cities 2012
+ */
+?><?php
+$TRACE_ENABLED = is_user_logged_in();
+$TRACE_PREFIX = 'pods-media-archive-search';
+$pods_toplevel_ancestor = 306;
+?><?php get_header(); ?>
+
+<div role="main">
+  <?php if ( have_posts() ) : the_post(); endif; ?>
+  <div id="post-<?php the_ID(); ?>" <?php post_class('lc-article lc-media-archive-search'); ?>>
+    <div class='ninecol' id='contentarea'>
+      <div class='top-content'>
+        <?php if(count($heading_slides)) : ?>
+        <header class='heading-image'>
+          <div class='flexslider wireframe'>
+            <ul class='slides'>
+              <?php foreach($heading_slides as $slide): ?>
+              <li><img src='<?php echo $slide; ?>' /></li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+        </header>
+        <?php endif; ?>
+        
+        <article class='wireframe eightcol row'>
+          <header class='entry-header'>
+            <h1>Search media archive</h1>
+          </header>
+          <div class='entry-content article-text'>
+            <form action='' method='get'>
+              <input type='text' name='search' id='media-archive-search' />
+              <input type='submit' value='Search' />
+            </form>
+            
+            <div id='search-results'>
+            </div>
+          </div>
+        </article>
+        <aside class='wireframe fourcol last entry-meta' id='keyfacts'>
+          &nbsp;
+        </aside><!-- #keyfacts -->
+      </div><!-- .top-content -->
+      <div class='extra-content twelvecol'>
+      </div><!-- .extra-content -->
+    </div><!-- #contentarea -->
+  </div><!-- #post-<?php the_ID(); ?> -->
+
+</div><!-- role='main'.row -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
