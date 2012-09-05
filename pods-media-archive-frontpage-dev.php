4c425560fd45eb66bf3f673845f201c9c9aa0753
andrea rota <a.rota@lse.ac.uk>
2012-09-05 14:02:18 +0100

    prepared dev versions of templates

---
+++ pods-media-archive-frontpage-dev.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Template Name: Pods - Media Archive search (dev)
+ * Description: Media Archive search webapp (dev)
+ *
+ * @package LSE Cities 2012
+ */
+?><?php
+$TRACE_ENABLED = is_user_logged_in();
+$TRACE_PREFIX = 'pods-media-archive-search';
+$pods_toplevel_ancestor = 306;
+?><?php get_header(); ?>
+
+<div role="main">
+  <?php if ( have_posts() ) : the_post(); endif; ?>
+  <div id="post-<?php the_ID(); ?>" <?php post_class('lc-article lc-media-archive-search'); ?>>
+    <div class='ninecol' id='contentarea'>
+      <div class='top-content'>
+        <?php if(count($heading_slides)) : ?>
+        <header class='heading-image'>
+          <div class='flexslider wireframe'>
+            <ul class='slides'>
+              <?php foreach($heading_slides as $slide): ?>
+              <li><img src='<?php echo $slide; ?>' /></li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+        </header>
+        <?php endif; ?>
+        
+        <article class='wireframe eightcol row'>
+          <header class='entry-header'>
+            <h1>Search media archive</h1>
+          </header>
+          <div class='entry-content article-text'>
+            <p>
+              <form action='' method='get'>
+                <input type='text' name='search' id='query' />
+                <input type='submit' value='Search' id='searchbutton' />
+              </form>
+            </p>
+            
+            <div id='searchresults'>
+            </div>
+          </div>
+        </article>
+        <aside class='wireframe fourcol last entry-meta' id='keyfacts'>
+          &nbsp;
+        </aside><!-- #keyfacts -->
+      </div><!-- .top-content -->
+      <div class='extra-content twelvecol'>
+      </div><!-- .extra-content -->
+    </div><!-- #contentarea -->
+  </div><!-- #post-<?php the_ID(); ?> -->
+<script src="https://raw.github.com/janl/mustache.js/master/mustache.js"></script>
+<script type="text/javascript">
+  // tests: http://jsfiddle.net/xswVa/1/
+  $ = jQuery;
+  
+  var mTemplate = '<h2>Search results</h2><ul>\
+  {{#items}}\
+    <li><a href="https://youtu.be/{{youtube_uri}}">{{title}}</a></li>\
+  {{/items}}\
+  </ul>';
+  
+  function runMediaQuery() {
+    var query = $('#query').val();
+    
+    if(query.length) {
+      var datastring = 'search=' + query;
+      
+      $.ajax(
+        {
+          type: "GET",
+          url: "/media/search",
+          data: datastring,
+          dataType: "json",
+          cache: false,
+          success: function(content, status) {
+            console.log('ajax status: ' + status + "\nsearch results: " + content);
+            $('#searchresults').html(Mustache.render(mTemplate, content));
+          }
+        }
+      );
+    } else {
+      $('#searchresults').html('');
+    }
+  }
+  
+  var typewatch = (function(){
+    var timer = 0;
+    return function(callback, ms){
+      clearTimeout (timer);
+      timer = setTimeout(callback, ms);
+    }  
+  })();
+
+  jQuery(document).ready(function($) {
+    $('#searchbutton').click(function(e) {
+      e.preventDefault();
+      runMediaQuery();
+    });
+    $('#query').keyup(function() {
+      typewatch(runMediaQuery, 500);
+    });
+  });
+</script>
+</div><!-- role='main'.row -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
---
+++ pods-media-archive-jsonsearch.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template Name: Media Archive item (JSON)
+ * Description: The template used to return a JSON view of a Media Archive object's metadata
+ *
+ * @package LSE Cities 2012
+ */
+?><?php
+/**
+ * Pods initialization
+ * URI: /media/search/?search=<search_string>
+ */
+$TRACE_ENABLED = is_user_logged_in();
+$PODS_BASEURI_MEDIA_ARCHIVE_SEARCH = '/media/search/';
+
+// setting search string from post meta is used in WP pages with hardcoded queries
+$search = get_post_meta($post->ID, 'pod_slug', true);
+
+// otherwise we expect the search string as a GET parameter
+if(!$search) {
+  $search = pods_url_variable('search', 'get');
+}
+
+if($TRACE_ENABLED) { error_log('pod_slug: ' . $pod_slug); }
+if($TRACE_ENABLED) { error_log('search: ' . $search); }
+
+$params = array(
+  'where' => 't.name LIKE "%' . $search . '%" OR session.speakers.family_name LIKE "%' . $search . '%" OR event.speakers.family_name LIKE "%' . $search . '%"'
+);
+$pod = new Pod('media_item_v0', $params);
+
+$media_items = array();
+
+while($pod->fetchRecord()) {
+  $media_item = array (
+    'id' => $pod->get_field('slug'),
+    'title' => $pod->get_field('name'),
+    'date' => $pod->get_field('date'),
+    'youtube_uri' => $pod->get_field('youtube_uri'),
+    'video_uri' => $pod->get_field('video_uri'),
+    'audio_uri' => $pod->get_field('audio_uri'),
+    'presentation_uri' => $pod->get_field('presentation_uri'),
+    'tags' => $pod->get_field('tag.name'),
+    'geotags' => $pod->get_field('geotags.name')
+  );
+  array_push($media_items, $media_item);
+}
+
+echo json_encode(array('items' => $media_items));
+?>
