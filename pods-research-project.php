d864a0535e3fb88ffc3a48bd4ba880a5177cdd01
andrea rota <a.rota@lse.ac.uk>
2012-09-19 11:14:21 +0100

    renamed contacts->coordinators

--- pods-research-project.php
+++ pods-research-project.php
@@ -60,22 +60,27 @@ if($pod->get_field('date_end')) {
   $project_end = $project_end->format('Y');
 }
 
-$project_contacts_list = $pod->get_field('contacts');
-foreach($project_contacts_list as $project_contact) {
-  $project_contacts .= $project_contact['name'] . ' ' . $project_contact['family_name'] . ', ';
+$project_coordinators_list = $pod->get_field('coordinators');
+$project_coordinators_count = count($project_coordinators_list);
+foreach($project_coordinators_list as $project_coordinator) {
+  $project_coordinators .= $project_coordinator['name'] . ' ' . $project_coordinator['family_name'] . ', ';
 }
-$project_contacts = substr($project_contacts, 0, -2);
+$project_coordinators = substr($project_coordinators, 0, -2);
 
 $project_researchers_list = $pod->get_field('researchers');
+$project_researchers_count = count($project_researchers_list);
 foreach($project_researchers_list as $project_researcher) {
   $project_researchers .= $project_researcher['name'] . ' ' . $project_researcher['family_name'] . ', ';
 }
 $project_researchers = substr($project_researchers, 0, -2);
+
 $project_partners_list = $pod->get_field('contributors');
+$project_partners_count = count($project_partners_list);
 foreach($project_partners_list as $project_partner) {
   $project_partners .= $project_partner['name'] . ', ';
 }
 $project_partners = substr($project_partners, 0, -2);
+
 $research_strand_title = $pod->get_field('research_strand.name');
 $research_strand_summary = $pod->get_field('research_strand.summary');
 
@@ -113,16 +118,16 @@ $project_status = $pod->get_field('project_status.name');
         </article>
         <aside class='wireframe fourcol last entry-meta' id='keyfacts'>
           <dl>
-          <?php if($project_contacts): ?>
-            <dt>Project contact</dt>
-            <dd><?php echo $project_contacts; ?></dd>
+          <?php if($project_coordinators): ?>
+            <dt>Project <?php echo $project_coordinators_count > 1 ?'coordinators' : 'coordinator'; ?></dt>
+            <dd><?php echo $project_coordinators; ?></dd>
           <?php endif; ?>
           <?php if($project_researchers): ?>
-            <dt>Researchers</dt>
+            <dt><?php echo $project_researchers_count > 1 ? 'Researchers' : 'Researcher'; ?></dt>
             <dd><?php echo $project_researchers; ?></dd>
           <?php endif; ?>
           <?php if($project_partners): ?>
-            <dt>Project partners</dt>
+            <dt>Project <?php echo $project_partners_count > 1 ? 'partners' : 'partner'; ?></dt>
             <dd><?php echo $project_partners; ?></dd>
           <?php endif; ?>
           <?php if($research_strand_title): ?>
