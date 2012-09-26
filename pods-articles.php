8001400203a42b1024e0bf9ae948d7e74e466457
andrea rota <a.rota@lse.ac.uk>
2012-09-26 16:09:04 +0100

    moved component to own file

--- pods-articles.php
+++ pods-articles.php
@@ -147,23 +147,7 @@ var_trace($gallery, 'gallery: ');
                       <?php echo $article_summary; ?>
                     <?php endif; ?>
                     </div>
-                    <?php if(count($gallery['slides'])): ?>
-                    <div class="lc-galleria" id="lc-galleria-<?php echo $gallery['slug']?>">
-                      <ul>
-                    <?php foreach($gallery['slides'] as $slide): ?>
-                      <li>
-                      <a href="<?php echo wp_get_attachment_url($slide->ID); ?>">
-                        <img title="<?php echo $slide->post_title; ?>"
-                          src="<?php echo wp_get_attachment_url($slide->ID); ?>"
-                          alt="<?php echo get_post_meta($slide->ID, '_wp_attachment_image_alt', true) ? get_post_meta($slide->ID, '_wp_attachment_image_alt', true) : $slide->post_title; ?>"
-                          data-title="<?php echo $slide->post_title; ?>" 
-                          data-description="<?php echo $slide->post_excerpt; ?>" />
-                      </a>
-                      </li>
-                    <?php endforeach; ?>
-                      </ul>
-                    </div>
-                    <?php endif; ?>
+                    <?php include('inc/components/galleria.php'); ?>
                     <?php if($article_extra_content): ?>
                     <div class="extra-content"><?php echo $article_extra_content; ?></div>
                     <?php endif; ?>
