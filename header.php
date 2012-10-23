67996d4ae2e6cf4ccc00f8345dbdaf426baf9f92
andrea rota <a.rota@lse.ac.uk>
2012-10-23 17:01:17 +0100

    added support for ie7

--- header.php
+++ header.php
@@ -96,6 +96,9 @@ if($post->ID == 2481) { // Labs -> Cities and the crisis
 <?php wp_enqueue_style('jquery-mediaelement', get_stylesheet_directory_uri() .'/stylesheets/mediaelement/mediaelementplayer.css'); ?>
 <?php wp_enqueue_style('font-theovandoegsburg', get_stylesheet_directory_uri() . '/stylesheets/fonts/theovand/stylesheet.css'); ?>
 <?php wp_enqueue_style('font-fontello', get_stylesheet_directory_uri() . '/stylesheets/fonts/fontello/stylesheet.css'); ?>
+<!--[if IE 7]>
+<?php wp_enqueue_style('font-fontello', get_stylesheet_directory_uri() . '/stylesheets/fonts/fontello/css/fontello-ie7.css'); ?>
+<!--[endif]-->
  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
