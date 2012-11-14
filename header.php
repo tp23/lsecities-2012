64020918f2e078aead364fd6ed9a75582119466f
andrea rota <a.rota@lse.ac.uk>
2012-11-14 10:02:51 +0000

    ec2012 pages are only for logged-in users atm

--- header.php
+++ header.php
@@ -9,7 +9,12 @@
 ?><?php
 
 if($_GET["siteid"] == 'ec2012') {
-  get_header('ec2012');
+  if(is_user_logged_in()) {
+    get_header('ec2012');
+  } else {
+    wp_redirect(home_url());
+    exit;
+  }
 } else {
   get_header('default');
 }
