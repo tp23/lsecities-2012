b1a0d901f9033e09e94ac5296a07e9c55074d4a2
andrea rota <a.rota@lse.ac.uk>
2012-11-21 15:02:33 +0000

    removing tabzilla for now

--- header-ec2012.php
+++ header-ec2012.php
@@ -111,12 +111,6 @@ if($post->ID == 2481 or in_array(2481, $post->ancestors)) { // Labs -> Cities an
 <?php wp_enqueue_style('jquery-mediaelement', get_stylesheet_directory_uri() .'/stylesheets/mediaelement/mediaelementplayer.css'); ?>
 <?php wp_enqueue_style('font-theovandoegsburg', get_stylesheet_directory_uri() . '/stylesheets/fonts/theovand/stylesheet.css'); ?>
 <?php wp_enqueue_style('font-fontello', get_stylesheet_directory_uri() . '/stylesheets/fonts/fontello/stylesheet.css'); ?>
-<?php
-/* tabzilla */
-wp_enqueue_style('module-tabzilla', get_stylesheet_directory_uri() . '/stylesheets/modules/tabzilla/tabzilla.css');
-wp_enqueue_script('module-tabzilla', get_stylesheet_directory_uri() . '/javascripts/modules/tabzilla/tabzilla.js', 'jquery', false, true);
-?>
-
 <!--[if IE 7]>
 <?php wp_enqueue_style('font-fontello', get_stylesheet_directory_uri() . '/stylesheets/fonts/fontello/css/fontello-ie7.css'); ?>
 <!--[endif]-->
