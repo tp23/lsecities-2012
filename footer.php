16c0770b7b126f2d03c5b2c9e1fe24cddab89a8e
andrea rota <a.rota@lse.ac.uk>
2012-11-22 10:52:13 +0000

    display list of conferences if in ec2012 mode

--- footer.php
+++ footer.php
@@ -44,6 +44,12 @@
           <ul>
             <?php wp_list_pages('title_li=&depth=1&child_of=94&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
           </ul>
+          <?php if($GLOBALS['site-ec2012']): ?>
+          <h1>Conferences</h1>
+          <ul>
+            <?php wp_list_pages('title_li=&depth=1&child_of=96&sort_column=menu_order&sort_order=ASC&echo=1'); ?>
+          </ul>          
+          <?php endif; ?>
         </div>
         <div class="twocol">
           <?php get_template_part('snippet-organizers'); ?>
