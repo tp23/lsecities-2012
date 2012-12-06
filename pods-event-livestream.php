8d678f1ad49cf346017d8cf75ead76b7fa431cea
andrea rota <a.rota@lse.ac.uk>
2012-12-06 06:43:23 +0000

    spelling

--- pods-event-livestream.php
+++ pods-event-livestream.php
@@ -73,7 +73,7 @@ foreach($live_storify_stories_uris as $story_uri) {
                 <?php endif; //($live_streaming_video_embedcode) ?>
                 <script>
                   jQuery(document).ready(function($){
-                    jQUery('iframe.twitter-timeline').load(function() {
+                    jQuery('iframe.twitter-timeline').load(function() {
                       jQuery('iframe.twitter-timeline').contents().find('.timeline-footer').remove();
                     });
                   });
