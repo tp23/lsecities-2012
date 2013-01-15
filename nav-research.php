e7a6bcc97cb4f9a8eb8f25a55cd9a80c6c050564
andrea rota <a.rota@lse.ac.uk>
2013-01-15 16:43:49 +0000

    updated variable names to more meaningful ones

--- nav-research.php
+++ nav-research.php
@@ -14,10 +14,10 @@ var_trace('post ID: ' . $current_post_id, $TRACE_PREFIX, $TRACE_ENABLED);
 var_trace(var_export($pod, true), $TRACE_PREFIX, $TRACE_ENABLED);
 
 
-$current_projects = compose_project_list_by_strand('active');
-var_trace(var_export($current_projects, true), $TRACE_PREFIX . ' - active projects (by strand): ', $TRACE_ENABLED);
-$past_projects = compose_project_list_by_strand('completed');
-var_trace(var_export($past_projects, true), $TRACE_PREFIX . ' - completed projects (by strand): ', $TRACE_ENABLED);
+$current_projects_by_strand = compose_project_list_by_strand('active');
+var_trace(var_export($current_projects_by_strand, true), $TRACE_PREFIX . ' - active projects (by strand): ', $TRACE_ENABLED);
+$past_projects_by_strand = compose_project_list_by_strand('completed');
+var_trace(var_export($past_projects_by_strand, true), $TRACE_PREFIX . ' - completed projects (by strand): ', $TRACE_ENABLED);
 
 ?>
   <?php if(($IN_CONTENT_AREA and !$HIDE_CURRENT_PROJECTS) or (!$IN_CONTENT_AREA and $HIDE_CURRENT_PROJECTS)): ?>
@@ -25,7 +25,7 @@ var_trace(var_export($past_projects, true), $TRACE_PREFIX . ' - completed projec
     <div id="current-projects">
       <?php if(!$IN_CONTENT_AREA): ?><h1>Current research</h1><?php endif; ?>
       <dl>
-      <?php foreach($current_projects as $strand): ?>
+      <?php foreach($current_projects_by_strand as $strand): ?>
         <dt><?php echo $strand['name']; ?></dt>
         <?php foreach($strand['projects'] as $strand_project): ?>
         <dd><a href="<?php echo $BASE_URI . '/' . $strand_project['slug']; ?>"><?php echo $strand_project['name']; ?></a></dd>
@@ -40,7 +40,7 @@ var_trace(var_export($past_projects, true), $TRACE_PREFIX . ' - completed projec
     <div id="past-projects">
       <?php if(!$IN_CONTENT_AREA): ?><h1>Completed research</h1><?php endif; ?>
       <dl>
-      <?php foreach($past_projects as $strand): ?>
+      <?php foreach($past_projects_by_strand as $strand): ?>
         <dt><?php echo $strand['name']; ?></dt>
         <?php foreach($strand['projects'] as $strand_project): ?>
         <dd><a href="<?php echo $BASE_URI . '/' . $strand_project['slug']; ?>"><?php echo $strand_project['name']; ?></a></dd>
