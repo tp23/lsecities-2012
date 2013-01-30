7ca997fff6c08ceb2de6e9aa264f93c90b8d4a67
andrea rota <a.rota@lse.ac.uk>
2013-01-30 14:48:25 +0000

    prepared configuration array

--- pages-people-list.php
+++ pages-people-list.php
@@ -24,8 +24,26 @@ $people_in_output_summary = array();
 
 $lists = array(
   'lsecities-staff' =>
-    array('lsecities-staff-mgmt'),
-    array('lsecities-staff')
+    array(
+      array('slug' => 'lsecities-staff-mgmt', 'label' => 'Executive'),
+      array('slug' => 'lsecities-staff', 'label' => 'Centre staff')
+    ),
+  'lsecities-advisory-board' =>
+    array(
+      array('slug' => 'lsecities-advisory-board-chair', 'label' => 'Chair'),
+      array('slug' => 'lsecities-advisory-board', 'label' => 'Advisors')
+    ),
+  'lsecities-executive-group' =>
+    array(
+      array('slug' => 'lsecities-executive-group-director', 'label' => 'Director'),
+      array('slug' => 'lsecities-executive-group-executive-director', 'label' => 'Executive director'),
+      array('slug' => 'lsecities-executive-group-academic-director', 'label' => 'Academic director')
+    ),
+  'lsecities-governing-board' =>
+    array(
+      array('slug' => 'lsecities-governing-board-chair', 'label' => 'Chair'),
+      array('slug' => 'lsecities-governing-board', 'label' => 'Board members')
+    )
 );
   
 function generate_list($list_id, $mode = MODE_FULL_LIST) {
