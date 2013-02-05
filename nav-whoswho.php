45759bc7a3f3fdd45233f4f1bff5c1044bee003c
andrea rota <a.rota@lse.ac.uk>
2013-02-05 12:39:33 +0000

    using dl for in-page nav

--- nav-whoswho.php
+++ nav-whoswho.php
@@ -5,7 +5,16 @@ $current_post_id = $post->ID;
 $people_list = get_post_meta($post->ID, 'people_list', true);
 if($people_list):
 ?>
-<nav id="whoswho-side-toc">
-<?php echo generate_list($people_list, 'summary'); ?>
-</nav>
+<?php if(is_user_logged_in()) : ?>
+<dl>
+  <dt><?php echo get_the_title($post->ID); ?></dt>
+  <dd>
+<?php endif; ?>
+    <nav id="whoswho-side-toc">
+    <?php echo generate_list($people_list, 'summary'); ?>
+    </nav>
+<?php if(is_user_logged_in()) : ?>
+  </dd>
+</dl>
+<?php endif; ?>
 <?php endif; ?>
