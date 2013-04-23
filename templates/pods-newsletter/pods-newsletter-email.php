76655c80825e0d8415cfcbdd3066960898ed8888
andrea rota <a.rota@lse.ac.uk>
2013-04-23 14:23:22 +0100

    moar tracing

--- templates/pods-newsletter/pods-newsletter-email.php
+++ templates/pods-newsletter/pods-newsletter-email.php
@@ -9,6 +9,8 @@
 // Set up the objects needed
 $all_sections_query = new WP_Query(array('post_type' => 'page', 'orderby' => 'menu_order'));
 
+var_trace(var_export($post->ID, true));
+var_trace(var_export($all_sections_query, true));
 // Find sections
 $sections = get_page_children( $post->ID, $all_sections_query );
 
