98f22700c788e2a113ccfc9c6905d2e8f56e7019
andrea rota <a.rota@lse.ac.uk>
2013-04-23 11:07:29 +0100

    testing lookup of sections

--- templates/pods-newsletter/pods-newsletter-email.php
+++ templates/pods-newsletter/pods-newsletter-email.php
@@ -5,4 +5,14 @@
  *
  * @package LSECities2012
  */
+?><?php
+// Set up the objects needed
+$my_wp_query = new WP_Query();
+$all_wp_pages = $my_wp_query->query(array('post_type' => 'page'));
+
+// Filter through all pages and find Portfolio's children
+$sections = get_page_children( $post->ID, $all_wp_pages );
+
+// echo what we get back from WP to the browser
+echo '<pre>' . print_r( $sections, true ) . '</pre>';
 ?>
