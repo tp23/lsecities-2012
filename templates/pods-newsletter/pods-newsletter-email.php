22d11f4f52d82c975822f3e900b48018ef9c175b
andrea rota <a.rota@lse.ac.uk>
2013-04-23 14:11:13 +0100

    tracing

--- templates/pods-newsletter/pods-newsletter-email.php
+++ templates/pods-newsletter/pods-newsletter-email.php
@@ -13,9 +13,12 @@ $all_sections_query = $my_query->query(array('post_type' => 'page', 'orderby' =>
 // Filter through all pages and find Portfolio's children
 $sections = get_page_children( $post->ID, $all_sections_query );
 
+var_trace(var_export($sections));
+
 foreach($sections as $section) {
   $featured_items_query = $my_query->query(array('post_type' => 'page', 'meta_key' => 'toc_title', 'orderby' => 'menu_order'));
   $featured_items = get_page_children($section->ID, $featured_items_query);
+  var_trace(var_export($featured_items));
   include('header-section-email.php');
 }
 ?>
