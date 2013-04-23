8abb0bfc1e35b017e266d50ff6cb87b50d71a112
andrea rota <a.rota@lse.ac.uk>
2013-04-23 15:00:36 +0100

    use get_pages in nested loop too

--- templates/pods-newsletter/pods-newsletter-email.php
+++ templates/pods-newsletter/pods-newsletter-email.php
@@ -10,6 +10,7 @@
 //$sections = new WP_Query(array('post_type' => 'page', 'orderby' => 'menu_order', 'post_parent'=>$post->ID));
 $sections = get_pages(array(
   'parent' => $post->ID,
+  'post_type' => 'page',
   'sort_column'  => 'menu_order',
   'hierarchical' => 0
 ));
@@ -18,7 +19,13 @@ var_trace(var_export($post->ID, true));
 var_trace(var_export($sections, true));
 
 foreach($sections as $section) {
-  $featured_items = new WP_Query(array('post_type' => 'page', 'meta_key' => 'toc_title', 'orderby' => 'menu_order', 'post_parent' => $section->ID));
+  $featured_items = get_pages(array(
+    'parent' => $section->ID,
+    'post_type' => 'page',
+    'sort_column'  => 'menu_order',
+    'meta_key' => 'toc_title',
+    'hierarchical' => 0
+  ));
   var_trace(var_export($featured_items, true));
   include('header-section-email.php');
 }
