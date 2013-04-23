f9a5e11b3ce67ca138bad8bf1b4861a657bad4b2
andrea rota <a.rota@lse.ac.uk>
2013-04-23 14:28:08 +0100

    removed extra step

--- templates/pods-newsletter/pods-newsletter-email.php
+++ templates/pods-newsletter/pods-newsletter-email.php
@@ -7,18 +7,13 @@
  */
 ?><?php
 // Set up the objects needed
-$all_sections_query = new WP_Query(array('post_type' => 'page', 'orderby' => 'menu_order'));
+$sections = new WP_Query(array('post_type' => 'page', 'orderby' => 'menu_order', 'post_parent'=>$post->ID));
 
 var_trace(var_export($post->ID, true));
-var_trace(var_export($all_sections_query, true));
-// Find sections
-$sections = get_page_children( $post->ID, $all_sections_query );
-
 var_trace(var_export($sections, true));
 
 foreach($sections as $section) {
-  $featured_items_query = new WP_Query(array('post_type' => 'page', 'meta_key' => 'toc_title', 'orderby' => 'menu_order'));
-  $featured_items = get_page_children($section->ID, $featured_items_query);
+  $featured_items = new WP_Query(array('post_type' => 'page', 'meta_key' => 'toc_title', 'orderby' => 'menu_order', 'post_parent' => $section->ID));
   var_trace(var_export($featured_items, true));
   include('header-section-email.php');
 }
