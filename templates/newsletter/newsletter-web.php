f017199c151590df2120290d38be264ed56a9285
andrea rota <a.rota@lse.ac.uk>
2013-04-30 12:16:31 +0100

    initial web channel newsletter template

--- templates/newsletter/newsletter-web.php
+++ templates/newsletter/newsletter-web.php
@@ -5,4 +5,25 @@
  *
  * @package LSECities2012
  */
+get_header(); ?>
+
+	<div id="primary" class="site-content ninecol">
+		<div id="content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+      <div class="extended-table-of-contents">
+<?php
+foreach($newsletter_sections as $section) {
+  include('header-section-email.php');
+}
 ?>
+      </div>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
