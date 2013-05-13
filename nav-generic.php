f3c456e7d4964cc4f1b3f31df5b7be1b1097830d
andrea rota <a.rota@lse.ac.uk>
2013-05-13 12:19:43 +0100

    do not rewrite sidebar links in labs section

--- nav-generic.php
+++ nav-generic.php
@@ -4,9 +4,10 @@ if($current_post_id) {
   $children = wp_list_pages('title_li=&depth=1&child_of='.$parent_post_id.'&echo=0');
   
   // if we are in the labs folder, hide lsecities hostname in links
+  /*
   if($current_post_id == 2481 or in_array(2481, get_post_ancestors($current_post_id))) {
     $children = hide_lsecities_page_hierarchy_in_labs_links($children);
-  }
+  }*/
 }
 if ($children) : ?>
 <nav>
