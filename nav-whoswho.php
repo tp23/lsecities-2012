a24c9c25969575012c3ead05e6880754ae906373
andrea rota <a.rota@lse.ac.uk>
2013-05-23 20:38:16 +0100

    update nav template to new pipeline

--- nav-whoswho.php
+++ nav-whoswho.php
@@ -1,8 +1,6 @@
 <?php
 $TRACE_ENABLED = is_user_logged_in();
 $TRACE_PREFIX = 'nav.php -- ';
-$current_post_id = $post->ID;
-$people_list = get_post_meta($post->ID, 'people_list', true);
 if($people_list):
 ?>
 <?php if(is_user_logged_in()) : ?>
