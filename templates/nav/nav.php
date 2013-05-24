34bf4274d31eee968f5bde782ef8464e98d83912
andrea rota <a.rota@lse.ac.uk>
2013-05-24 13:32:31 +0100

    switched to strict equals-to

--- templates/nav/nav.php
+++ templates/nav/nav.php
@@ -33,42 +33,42 @@ var_trace(var_export($pods_toplevel_ancestor, true), $TRACE_PREFIX . 'pods_tople
 $nav_generated = false;
 
 // / (main frontpage)
-if($current_post_id == 393) {
+if($current_post_id === 393) {
   get_template_part('templates/partials/lsecities-frontpage');
   $nav_generated = true;
 }
 
 // /ua/ (Urban Age frontpage)
-if($current_post_id == 94) {
+if($current_post_id === 94) {
   get_template_part('templates/partials/organizers');
   $nav_generated = true;
 }
 
 // /research (the whole Research section) or individual Research project pod items
-if($current_post_id == 306 or in_array(306, get_post_ancestors($current_post_id)) or (lc_data('pods_toplevel_ancestor') == 306)) {
+if($current_post_id === 306 or in_array(306, get_post_ancestors($current_post_id)) or (lc_data('pods_toplevel_ancestor') === 306)) {
   get_template_part('templates/nav/nav', 'research');
   $nav_generated = true;
 }
 
 // /publications (the whole Publications section)
-if($current_post_id == 309 or in_array(309, get_post_ancestors($current_post_id))) {
+if($current_post_id === 309 or in_array(309, get_post_ancestors($current_post_id))) {
   get_template_part('templates/nav/nav', 'publications');
   $nav_generated = true;
 }
 
-if(lc_data('pods_toplevel_ancestor') == 309) {
+if(lc_data('pods_toplevel_ancestor') === 309) {
   get_template_part('templates/nav/nav', 'article');
   $nav_generated = true;
 }
 
 // /events (the whole Events section) or individual Event pod items
-if($current_post_id == 311 or in_array(311, get_post_ancestors($current_post_id)) or (lc_data('pods_toplevel_ancestor') == 311)) {
+if($current_post_id === 311 or in_array(311, get_post_ancestors($current_post_id)) or (lc_data('pods_toplevel_ancestor') === 311)) {
   get_template_part('templates/nav/nav', 'events');
   $nav_generated = true;
 }
 
 // /ua/award/ or /about/collaboration-opportunities/
-if($current_post_id == 489 or in_array(1890, get_post_ancestors($current_post_id))) {
+if($current_post_id === 489 or in_array(1890, get_post_ancestors($current_post_id))) {
   get_template_part('templates/nav/nav', 'empty');
   $nav_generated = true;
 }
@@ -80,7 +80,7 @@ if(lc_data('nav_show_conferences') or check_parent_conference(191) or check_pare
 }
 
 // /about/whos-who/
-if($current_post_id == 421 or in_array(421, get_post_ancestors($current_post_id))) {
+if($current_post_id === 421 or in_array(421, get_post_ancestors($current_post_id))) {
   // first display navigation for this section
   get_template_part('templates/nav/nav', 'generic');
   
@@ -90,12 +90,12 @@ if($current_post_id == 421 or in_array(421, get_post_ancestors($current_post_id)
 }
 
 // /urban-at-lse/
-if($current_post_id == 3338 or in_array(3338, get_post_ancestors($current_post_id))) {
+if($current_post_id === 3338 or in_array(3338, get_post_ancestors($current_post_id))) {
   get_template_part('templates/nav/nav', 'urbanatlse');
   $nav_generated = true;
 }
 
-if($nav_generated == false) {
+if($nav_generated === false) {
   get_template_part('templates/nav/nav', 'generic');
 }
 
@@ -106,7 +106,7 @@ get_template_part('templates/nav/nav', 'mailing-list-subscription');
 <?php else: // (!lc_data('site-ec2012')) ?>
 <div class="wireframe threecol last" id="navigationarea">
 <?php
-if(lc_data('pods_toplevel_ancestor') == 309) {
+if(lc_data('pods_toplevel_ancestor') === 309) {
   get_template_part('templates/nav/nav', 'article');
   $nav_generated = true;
 } ?>
