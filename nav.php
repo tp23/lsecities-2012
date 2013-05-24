9cad6bc56c72b25d910d624dfd977c55ba6c5c58
andrea rota <a.rota@lse.ac.uk>
2013-05-24 09:40:39 +0100

    moved mailing list subscription part to own template

--- nav.php
+++ nav.php
@@ -94,29 +94,10 @@ if($current_post_id == 3338 or in_array(3338, get_post_ancestors($current_post_i
 if($nav_generated == false) {
   get_template_part('templates/nav/nav', 'generic');
 }
+
+// include mailing list subscription template part
+get_template_part('templates/nav/nav', 'mailing-list-subscription');
 ?>
-            
-  <div id="mailing-list-subscription">
-    <h1>Click here to subscribe to LSE Cities updates</h1>
-    <form method="post" action="http://urban-age.us4.list-manage1.com/subscribe/post" id="mailchimp-form">
-      <input type="hidden" value="6a19b1b794ce991fff919b68d" name="u" />
-      <input type="hidden" value="1f3b65491d" name="id" />
-      <label for="MERGE0">email address <em>*</em></label>
-      <input type="email" value="" name="MERGE0" id="MERGE0" />
-      <label for="MERGE1">first name <em>*</em></label>
-      <input type="text" value="" name="MERGE1" id="MERGE1" />
-      <label for="MERGE2">last name <em>*</em></label>
-      <input type="text" value="" name="MERGE2" id="MERGE2" />
-      <p><input type="submit" value="subscribe"/></p>
-    </form>
-  </div>
-  <script>
-    //<![CDATA[
-    jQuery(document).ready(function($) {
-      $('#mailing-list-subscription > h1').click(function() {$('#mailchimp-form').toggle('fast')});
-    });
-    //]]>
-  </script>
 </div>
 <?php else: // (!lc_data('site-ec2012')) ?>
 <div class="wireframe threecol last" id="navigationarea">
---
+++ templates/nav/nav-mailing-list-subscription.php
@@ -0,0 +1,21 @@
+<div id="mailing-list-subscription">
+<h1>Click here to subscribe to LSE Cities updates</h1>
+<form method="post" action="http://urban-age.us4.list-manage1.com/subscribe/post" id="mailchimp-form">
+  <input type="hidden" value="6a19b1b794ce991fff919b68d" name="u" />
+  <input type="hidden" value="1f3b65491d" name="id" />
+  <label for="MERGE0">email address <em>*</em></label>
+  <input type="email" value="" name="MERGE0" id="MERGE0" />
+  <label for="MERGE1">first name <em>*</em></label>
+  <input type="text" value="" name="MERGE1" id="MERGE1" />
+  <label for="MERGE2">last name <em>*</em></label>
+  <input type="text" value="" name="MERGE2" id="MERGE2" />
+  <p><input type="submit" value="subscribe"/></p>
+</form>
+</div>
+<script>
+//<![CDATA[
+jQuery(document).ready(function($) {
+  $('#mailing-list-subscription > h1').click(function() {$('#mailchimp-form').toggle('fast')});
+});
+//]]>
+</script>
