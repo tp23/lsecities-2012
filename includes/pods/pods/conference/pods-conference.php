a54bf2ae444750540c78bd144c3a3107ab338b3c
andrea rota <a.rota@lse.ac.uk>
2013-05-24 20:52:48 +0100

    dump all post meta items

--- includes/pods/pods/conference/pods-conference.php
+++ includes/pods/pods/conference/pods-conference.php
@@ -93,7 +93,7 @@ function parent_conference_page($post_id) {
   var_trace(var_export($ancestor_pages, true), 'fn:parent_conference_page -- ancestor_pages');
   
   foreach($ancestor_pages as $page) {
-    var_trace($page->ID . ':' . get_post_meta($page->ID, '_wp_page_template', true), '_wp_page_template');
+    var_trace($page->ID . ':' . get_post_meta($page->ID), 'post_meta');
     if(lc_data('pods_conference__wp_page_template') === get_post_meta($page->ID, '_wp_page_template', true)) {
       return array('id' => $page->ID, 'slug' => $page->post_name);
     }
